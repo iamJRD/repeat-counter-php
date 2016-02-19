f7493dd3bac6fcf9c01416290604a3df1034f96f
Jared Beckler <jared.beckler@gmail.com>
2016-02-19 10:33:56 -0800

    add twig template and routing for results page

--- app/app.php
+++ app/app.php
@@ -2,6 +2,9 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RepeatCounter.php";
 
+
+    session_start();
+
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"));
@@ -10,6 +13,12 @@
         return $app["twig"]->render("home.html.twig");
     });
 
+    $app->get("/results", function() use ($app) {
+        $new_RepeatCounter = new RepeatCounter($_GET["input_string"], $_GET["input_check_repeat"]);
+        $result = $new_RepeatCounter->countRepeats($_GET["input_string"], $_GET["input_check_repeat"]);
+
+        return $app["twig"]->render("results.html.twig", array("results" => $result, "originalString" => $_GET["input_string"], "originalCheckRepeat" => $_GET["input_check_repeat"]));
+    });
 
     return $app;
 ?>
