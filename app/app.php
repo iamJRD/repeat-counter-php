ca6cafd87f26a17a5022349f6f61fab21cb39963
Jared Beckler <jared.beckler@gmail.com>
2016-02-19 08:24:35 -0800

    add initial project files

---
+++ app/app.php
@@ -0,0 +1,10 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"));
+
+    return $app;
+?>
