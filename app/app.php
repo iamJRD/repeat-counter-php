5841613430098223ab70295b98d469cf8cef5159
Jared Beckler <jared.beckler@gmail.com>
2016-02-19 11:32:47 -0800

    remove unneeded spacing

--- app/app.php
+++ app/app.php
@@ -2,9 +2,6 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RepeatCounter.php";
 
-
-    session_start();
-
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"));
