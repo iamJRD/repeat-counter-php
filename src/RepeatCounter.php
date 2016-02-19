5a6f22588c81206fa401ff23636a022d28e49c90
Jared Beckler <jared.beckler@gmail.com>
2016-02-19 08:49:13 -0800

    add passing spec for when repeat check word does not appear in test string

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,3 +1,13 @@
 <?php
 
+    class RepeatCounter
+    {
+        function countRepeats($input_string, $input_check_repeat)
+        {
+            if ($input_string != $input_check_repeat)
+            {
+                return "0";
+            }
+        }
+    }
 ?>
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -1,3 +1,17 @@
 <?php
     require_once "src/RepeatCounter.php";
+
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase
+    {
+        function test_no_repeat()
+        {
+            $test_RepeatCounter = new RepeatCounter;
+            $input_string = "a";
+            $input_check_repeat = "b";
+
+            $result = $test_RepeatCounter->countRepeats($input_string, $input_check_repeat);
+
+            $this->assertEquals("0", $result);
+        }
+    }
 ?>
